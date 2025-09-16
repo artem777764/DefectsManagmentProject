5eb3a75c2f10849a41ca6d099df57387948bceec
artem777764 <artem200513286@gmail.com>
2025-09-16 15:21:12 +0300

    Initial Frontend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-blue-500 bg-gray-100 p-4 rounded-lg">
+    Hello World Guys
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
